5aeb717e9f6492a0242c21b5a2d6ebdd7dff6bc1
Abtinz <abtinzandi@gmail.com>
2022-02-10 13:00:54 +0330

    matrix and arrays

---
+++ ArraysMatrix/ArraysMatrix.m
@@ -0,0 +1,31 @@
+## Copyright (C) 2022 ABTIN
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {@var{retval} =} ArraysMatrix (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: ABTIN <ABTIN@HMD>
+## Created: 2022-02-10
+
+function retval = ArraysMatrix ()
+  
+  array = [1,2,3,4,5];
+  print(array);
+  
+
+endfunction
